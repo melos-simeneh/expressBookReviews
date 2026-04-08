e8d08fe6cbae4fdcbe1414c17da242bc69a58400
Melos <melos.simeneh@gmail.com>
2026-04-08 09:08:15 +0300

    Complete Task 4 - get books by title

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -50,8 +50,20 @@ public_users.get('/author/:author',function (req, res) {
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const { title } = req.params;
+  const booksByTitle = {};
+
+  Object.keys(books).forEach((isbn) => {
+    if (books[isbn].title.toLowerCase() === title.toLowerCase()) {
+      booksByTitle[isbn] = books[isbn];
+    }
+  });
+
+  if (Object.keys(booksByTitle).length === 0) {
+    return res.status(404).json({ message: "No books found for the given title." });
+  }
+
+  return res.status(200).json(booksByTitle);
 });
 
 //  Get book review
