8a6bb1a4faf545420aa520fa275664fa6dc57311
Melos <melos.simeneh@gmail.com>
2026-04-08 09:06:23 +0300

    Complete Task 3 - get books by author

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -32,8 +32,20 @@ public_users.get('/isbn/:isbn',function (req, res) {
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const { author } = req.params;
+  const booksByAuthor = {};
+
+  Object.keys(books).forEach((isbn) => {
+    if (books[isbn].author.toLowerCase() === author.toLowerCase()) {
+      booksByAuthor[isbn] = books[isbn];
+    }
+  });
+
+  if (Object.keys(booksByAuthor).length === 0) {
+    return res.status(404).json({ message: "No books found for the given author." });
+  }
+
+  return res.status(200).json(booksByAuthor);
 });
 
 // Get all books based on title
